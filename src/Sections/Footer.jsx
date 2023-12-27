2760d58925bb003b481eb4ac10e5aff3d058eaba
görkem serin <gorkemserin@outlook.com>
2023-12-27 18:23:54 +0300

    Favicon and Footer Updates

--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -2,15 +2,15 @@ export default function Footer(){
     return(
         <>
             <div class="flex sm:gap-2">
-                <p class="text-right">Copyright &copy; <span id="year">{new Date().getFullYear()}</span></p>
-                <p class="text-right">All Rights Reserved</p>
+                <p class="text-right mx-2 text-xs sm:text-lg">Copyright &copy; <span id="year">{new Date().getFullYear()}</span></p>
+                <p class="text-right text-xs sm:text-lg">All Rights Reserved</p>
             </div>
-            <nav class="flex gap-2" aria-label="footer">
-                <a href="/" class="hover:opacity-90">Terms & Conditions</a>
-                <a href="/" class="hover:opacity-90">Privacy</a>
-                <a href="/" class="hover:opacity-90">Manage Cookies</a>
-                <a href="/" class="hover:opacity-90">Security</a>
-                <a href="/" class="hover:opacity-90">Contact</a>
+            <nav class="flex gap-1 justify-center items-center mt-3" aria-label="footer">
+                <a href="/" class="hover:opacity-90 text-[10px] w-14 sm:w-36 sm:text-base">Terms & Conditions</a>
+                <a href="/" class="hover:opacity-90 text-[10px] w-14 sm:w-36 sm:text-base">Privacy</a>
+                <a href="/" class="hover:opacity-90 text-[10px] w-14 sm:w-36 sm:text-base">Manage Cookies</a>
+                <a href="/" class="hover:opacity-90 text-[10px] w-14 sm:w-36 sm:text-base">Security</a>
+                <a href="/" class="hover:opacity-90 text-[10px] w-14 sm:w-36 sm:text-base">Contact</a>
             </nav>
         </>
     )
